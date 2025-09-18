79b3f154d4e0a362655d6fe92d0b426501e7ab5c
Ashirwad Baniyal <ashirwadbaniyal789@gmail.com>
2025-09-18 13:22:27 +0530

    Initial commit

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+const Login = ({handleLogin}) => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const submitHandler = (e)=>{
+        e.preventDefault()
+        handleLogin(email,password)
+        setEmail("")
+        setPassword("")
+    }
+
+  return (
+    <div className=' flex h-screen w-screen items-center justify-center'>
+        <div className='text-white border-2 rounded-xl border-fuchsia-600 p-10'>
+        <h1 class=" mb-4 text-3xl font-bold  leading-none text-gray-200
+                   ">Login</h1>
+            <form 
+            onSubmit={(e)=>{
+                submitHandler(e)
+            }}
+            className='flex flex-col items-center justify-center'
+            >
+                 
+                <input 
+                value={email}
+                onChange={(e)=>{
+                    setEmail(e.target.value)
+                }}
+                required 
+                className='text-gray-700 outline-none bg-transparent border-2 border-fuchsia-500 font-medium text-lg py-2 px-6 rounded-full placeholder:text-gray-400' type="email" placeholder='Enter your email' 
+                />
+               
+                <input
+                value={password}
+                onChange={(e)=>{
+                    setPassword(e.target.value)
+                }}
+                required 
+                className='outline-none bg-transparent border-2 border-fuchsia-500 font-medium text-lg py-2 px-6 rounded-full mt-3 placeholder:text-gray-400 text-gray-700' type="password" placeholder='Enter password' />
+                <button className='mt-7 text-white border-none outline-none hover:bg-[#7449a6]  font-semibold bg-[#8756bd] text-lg py-2 px-8 w-full rounded-full placeholder:text-white'>Log in</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
